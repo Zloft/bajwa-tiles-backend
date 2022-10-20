c4fc6ca55f843fcd94accb8440b4a61b07f0bf56
Abdul Rehman <mehars.6925@gmail.com>
2022-10-21 01:05:22 +0500

    updated the index controller

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -7,24 +7,6 @@ import { Controller, Get, Req, Res } from 'routing-controllers';
 export class IndexController {
 @Get('/')
   async getApi(@Req() request: any, @Res() response: Response) {
-
-    const invoice = await Invoice.create({
-      id: uuidV4(),
-      customerName: 'Abdul Rehman',
-      customerPhone: '+92 316 7943024',
-      netPayable: 5000,
-      productMeasurements: '20x20',
-      productName: 'Master Tiles',
-      productPrice: 500,
-      productQuantity: 10,
-      tenantId: uuidV4(),
-    });
-
-    console.log(invoice, 'invoice created');
-    
-    
-    return response.json({
-      invoice
-    });
+    return response.send('Application is live...');
   }
 }
