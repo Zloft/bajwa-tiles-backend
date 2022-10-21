5f1467624ec0090ace71c72cbff02e3b05acf84c
Abdul Rehman <mehars.6925@gmail.com>
2022-10-21 18:37:55 +0500

    updated the invoice validator to validate for payments as well

--- src/validators/invoice.validator.ts
+++ src/validators/invoice.validator.ts
@@ -1,10 +1,26 @@
 import * as Joi from 'joi';
+import messages from '../config/messages';
+import { PAYMENT_TYPES } from '../config/constants';
 
 export const invoiceSchema = Joi.object({
   productName: Joi.string().trim(true).required(),
   customerName: Joi.string().trim(true).required(),
   customerPhone: Joi.string().trim(true).required(),
-  productPrice: Joi.number().required(),
-  productQuantity: Joi.number().required(),
+  productPrice: Joi.number().not(0).required(),
+  productQuantity: Joi.number().not(0).required(),
   productMeasurements: Joi.string().trim(true).required(),
+  payments: Joi.array().items({
+    paymentType: Joi.string().required().equal(...PAYMENT_TYPES),
+    amount: Joi.number().required(),
+  }).min(1).required()
+}).custom((value, helper) => {  
+  const { productPrice, productQuantity, payments } = value;
+  const netPayable = productPrice * productQuantity;
+
+  const amountsArray: number[] = payments.map((payment: any) => payment.amount);
+  const totalPaid = amountsArray.reduce((total, amount) => total + amount, 0);
+
+  if (totalPaid != netPayable)  throw new Error(messages.VALIDATION.INVOICE.PAYMENTS);
+
+  return {...value, netPayable};
 });
